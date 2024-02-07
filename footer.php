892cced0288a025824526527b834f597a7e43e51
ElvijsKurss <elvijs.kurss@gmail.com>
2024-02-07 14:03:53 +0200

    footer update 1

--- footer.php
+++ footer.php
@@ -1,12 +1,61 @@
 <footer class="footer text-center py-2 theme-bg-dark">
     <div id="footer-container">
 
-        <div class="column one">123</div>
-        <div class="column two">123</div>
-        <div class="column three">123</div>
-        <div class="column four">123</div>
-        <div class="column five">123</div>
+        <div class="column one">
+            <a href="#">
+                <img class="mb-3 mx-100px logo" src="wp-content/themes/libidiol/master-template/assets/images/logo.png" alt="logo">
+            </a>
+            <p class="footer-content-text">Unleash Natures Power For Passionate Living with Libidiol</p>
+            <ul class="social-icons">
+                <li class=""><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li class=""><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li class=""><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+                <li class=""><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+        <div class="column two">
+            <div id="column-two-content">
+                <h3>Useful Links</h3>
+            </div>
+            <div><a href="#">
+                    <p class="footer-content-text">About us</p>
+                </a></div>
+            <div><a href="#">
+                    <p class="footer-content-text">Delivery Information</p>
+                </a></div>
+            <div><a href="#">
+                    <p class="footer-content-text">Terms & Conditions</p>
+                </a></div>
+            <div><a href="#">
+                    <p class="footer-content-text">Privacy Policy</p>
+                </a></div>
+            <div><a href="#">
+                    <p class="footer-content-text">Refund Policy</p>
+                </a></div>
+        </div>
+        <div class="column three">
+            <div id="column-three-content">
+                <h3>Contact Us</h3>
+            </div>
+            <div><a href="#">
+                    <p class="footer-content-text">Thailand, Ramkhamhaeng road</p>
+                </a></div>
+            <div><a href="#">
+                    <p class="footer-content-text">+66858576671</p>
+                </a></div>
+            <div><a href="#">
+                    <p class="footer-content-text">office@libidiol.com</p>
+                </a></div>
 
+        </div>
+        <div class="column four">
+            <div id="column-four-content">
+                <h3>Send Your Message</h3>
+            </div>
+            <div><input placeholder="Your E-mail"></input></div>
+            <div><input placeholder="Text field"></input></div>
+            <div><button>Submit</button></div>
+        </div>
 
     </div>
 </footer>
