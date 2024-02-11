21390147f8271af454ae5fd400a9062bc9c1a0fb
ElvijsKurss <elvijs.kurss@gmail.com>
2024-02-11 12:56:46 +0200

    wordpress push

--- page.php
+++ page.php
@@ -4,8 +4,8 @@ get_header();
 
 ?>
 
-<h1><?= $post->post_title; ?>123</h1>
-<div><?= $post->post_content; ?>123</div>
+<!-- <h1><?= $post->post_title; ?>123</h1>
+<div><?= $post->post_content; ?>123</div> -->
 
 <?php
 get_footer();
