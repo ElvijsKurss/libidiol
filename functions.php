c01d8bfccc394d0ae0ce304bc76cd1e780f3eb77
ElvijsKurss <elvijs.kurss@gmail.com>
2024-02-26 14:23:25 +0200

    update

--- functions.php
+++ functions.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function libidiol_theme_support()
 {
     // manage WordPress title
@@ -24,9 +23,10 @@ add_action('init', 'libidiol_menus');
 function libidiol_register_styles()
 {
     $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('libidiol-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
+    wp_enqueue_style('libidiol-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
     wp_enqueue_style('libidiol-style', get_template_directory_uri() . "/assets/css/style.css", array('libidiol-bootstrap'), $version, 'all');
-    wp_enqueue_style('libidiol-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '1.0', 'all');
-    wp_enqueue_style('libidiol-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '1.0', 'all');
+    wp_enqueue_style('libidiol-style', get_template_directory_uri() . "/assets/css/fontello.css", array('libidiol-bootstrap'), $version, 'all');
 }
 
 add_action('wp_enqueue_scripts', 'libidiol_register_styles');
@@ -34,10 +34,12 @@ add_action('wp_enqueue_scripts', 'libidiol_register_styles');
 function libidiol_register_admin_styles()
 {
     $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('libidiol-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
+    wp_enqueue_style('libidiol-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
     wp_enqueue_style('libidiol-style', get_template_directory_uri() . "/assets/css/style.css", array('libidiol-bootstrap'), $version, 'all');
-    wp_enqueue_style('libidiol-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '1.0', 'all');
-    wp_enqueue_style('libidiol-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '1.0', 'all');
+    wp_enqueue_style('libidiol-style', get_template_directory_uri() . "/assets/css/fontello.css", array('libidiol-bootstrap'), $version, 'all');
 }
+
 
 // comment this line to disable additional admin stylesheets
 add_action('admin_enqueue_scripts', 'libidiol_register_admin_styles');
@@ -46,14 +48,9 @@ add_action('admin_enqueue_scripts', 'libidiol_register_admin_styles');
 function libidiol_register_scripts()
 {
     $version = wp_get_theme()->get('Version');
-    wp_enqueue_script('libidiol-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
-    wp_enqueue_script('libidiol-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
-    wp_enqueue_script('libidiol-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '3.4.1', true);
+    wp_enqueue_script('libidiol-jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js', array(), '3.7.1', true);
+    wp_enqueue_script('libidiol-bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js', array('libidiol-jquery'), '3.4.1', true);
     wp_enqueue_script('libidiol-main', get_template_directory_uri() . '/assets/js/main.js', array(), $version, true);
 }
 
 add_action('wp_enqueue_scripts', 'libidiol_register_scripts');
-
-?>
-
-<?php
--- header.php
+++ header.php
@@ -12,14 +12,12 @@ wp_head();
 
 </head>
 <header class="header text-center" id="home">
-    <nav class="navbar navbar-expand-lg navbar-dark main-wrapper">
+    <nav class="navbar navbar-expand-lg navbar-dark main-wrapper container">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-            <!--<button class="navbar-toggler" type="button" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">-->
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <!-- /* collapse show class makes it work semi-fine*/ -->
-        <div id="navigation" class="main-wrapper">
+        <div id="navigation" class="main-wrapper collapse">
             <div id="nav-bar-logo">
                 <a href="http://custom-theme-website.test/">
                     <img class="top-logo" src="<?= get_stylesheet_directory_uri() ?>/assets/images/logo.png" alt="logo" />
@@ -39,18 +37,46 @@ wp_head();
                 ?>
             </div>
 
+            <div class="mobile-contact-us-container">
+                <div><a href="#">
+                        <p class="header-content-text-mobile">Thailand, Ramkhamhaeng road</p>
+                    </a></div>
+                <div><a href="#">
+                        <p class="header-content-text-mobile">+66858576671</p>
+                    </a></div>
+                <div><a href="#">
+                        <p class="header-content-text-mobile">office@libidiol.com</p>
+                    </a></div>
+
+            </div>
+
+            <div class="mobile-social-icons-container">
+                <ul class="mobile-social-icons-container-content">
+                    <li class="footer-icons"><a href="#"><i class="fab fa-facebook" aria-hidden="true"></i></a></li>
+                    <li class="footer-icons"><a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
+                    <li class="footer-icons"><a href="#"><i class="fab fa-pinterest" aria-hidden="true"></i></a></li>
+                    <li class="footer-icons"><a href="#"><i class="fab fa-linkedin" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+
             <div id="nav-bar-icons">
-                <ul class="social-list">
+                <!-- <ul class="social-list">
                     <li class="list-inline-item"><a href="#"><i class="fa fa-chevron-down" aria-hidden="true"></i><img src="wp-content/themes/libidiol/master-template/assets/images/euro.png"></a></li>
                     <li class="list-inline-item"><a href="#"><i class="fa fa-chevron-down" aria-hidden="true"></i><img src="wp-content/themes/libidiol/master-template/assets/images/language.png"></a></li>
                     <li class="list-inline-item"><a href="#"><img src="wp-content/themes/libidiol/master-template/assets/images/search-symbol.png"></a></li>
                     <li class="list-inline-item"><a href="#"><img src="wp-content/themes/libidiol/master-template/assets/images/basket.png"></a></li>
+                </ul> -->
+                <ul class="social-list">
+                    <li class="list-inline-item"><a href="#"><i class="fa fa-chevron-down" aria-hidden="true"></i><i class="icon-euro menu-icon"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="fa fa-chevron-down" aria-hidden="true"></i><i class="icon-language menu-icon"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="icon-search menu-icon"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="icon-shopping-basket menu-icon"></i></a></li>
                 </ul>
             </div>
             <div class="head-button-container">
                 <div class="head-button-body"><a href="#order-now-section" class="header-button-link">Order now</a></div>
             </div>
         </div>
-
     </nav>
+
 </header>
