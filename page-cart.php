215354d3dddb9437663960f87d82c10609e4e15b
ElvijsKurss <elvijs.kurss@gmail.com>
2024-03-26 20:34:38 +0200

    Push

--- page-cart.php
+++ page-cart.php
@@ -3,18 +3,20 @@ get_header();
 ?>
 
 <body>
-    <div class="container main-wrapper container-mobile">
-        <?php
-        if (have_posts()) :
-            while (have_posts()) :
-                the_post();
-        ?>
-                <h1 class="custome-page-header"><?php the_title(); ?></h1>
-                <div class="custome-page-paragraph"><?php the_content(); ?></div>
-        <?php
-            endwhile;
-        endif;
-        ?>
+    <div class="container main-wrapper container-mobile-cart">
+        <div class="full-view-background-mobile">
+            <?php
+            if (have_posts()) :
+                while (have_posts()) :
+                    the_post();
+            ?>
+                    <h1 class="custome-page-header"><?php the_title(); ?></h1>
+                    <div class="custome-page-paragraph"><?php the_content(); ?></div>
+            <?php
+                endwhile;
+            endif;
+            ?>
+        </div>
     </div>
 
 </body>
