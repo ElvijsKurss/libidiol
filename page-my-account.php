0d44b0cc596ce69329bdadf7a8ebd83cb2bd6131
ElvijsKurss <elvijs.kurss@gmail.com>
2024-03-27 15:42:58 +0200

    push

---
+++ page-my-account.php
@@ -0,0 +1,26 @@
+<?php
+get_header();
+?>
+
+<body>
+    <div class="container main-wrapper container-mobile-cart">
+        <div class="full-view-background-mobile">
+            <?php
+            if (have_posts()) :
+                while (have_posts()) :
+                    the_post();
+            ?>
+                    <h1 class="custome-page-header"><?php the_title(); ?></h1>
+                    <div class="additional-custome-page-paragraph"><?php the_content(); ?></div>
+            <?php
+                endwhile;
+            endif;
+            ?>
+        </div>
+    </div>
+
+</body>
+
+<?php
+get_footer();
+?>
